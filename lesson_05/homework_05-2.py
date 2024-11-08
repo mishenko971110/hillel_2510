15e61850095ac94507a975e674feb3bd5790f5a5
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-11-08 22:04:01 +0200

    hw-5-tuples-dicts-lists
    There are tasks 1 and 2

---
+++ lesson_05/homework_05-2.py
@@ -0,0 +1,48 @@
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+# have age >=30. Print condition check result
+
+people_records = [
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+print('\nInitial list:\n', people_records)
+
+new_record = ('Anna', 'Verde', 26, 'QA Engineer', 'Ukraine')
+people_records.insert(0, new_record)
+
+record_temp = people_records[1]
+people_records[1] = people_records[5]
+people_records[5] = record_temp
+
+print('\nModified list:\n', people_records)
+
+index_list = (6, 10, 13)
+selected_people = [record for record in people_records if people_records.index(record) in index_list]
+
+is_over_30 = True
+
+for person_info in selected_people:
+    if person_info[2] <= 30:
+        is_over_30 = False
+        print(f'{person_info[0]} is younger than 30 years old.')
+        break
+
+if is_over_30 == True:
+    print('All people in the list are over 30 years old.')

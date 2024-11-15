1a4d0b8214b8dc6ec95b9006234c765b3cadb021
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-11-15 18:20:48 +0200

    add fix after review

--- lesson_06/homework_06-1.py
+++ lesson_06/homework_06-1.py
@@ -2,7 +2,4 @@
 # Якщо їх більше 10 - вивести в консоль True, інакше - False. 
 # Строку отримати за допомогою функції input()
 
-str = input('Input a string for check: ')
-count_uniq = len(set(str.lower()))
-
-print(count_uniq > 10)
+print(len(set(input('Input a string for check: ').lower())) > 10)

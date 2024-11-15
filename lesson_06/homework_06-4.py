ac71f56f76a8f74b2fa9b5f470642a9bca6c2439
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-11-15 14:10:17 +0200

    add fix

--- lesson_06/homework_06-4.py
+++ lesson_06/homework_06-4.py
@@ -3,12 +3,7 @@
 import random
 
 rand_list = [random.randint(0, 100) for _ in range(10)]
-sum = 0
-
 print('Random list: ', rand_list)
 
-for i in rand_list:
-    if i % 2 == 0:
-        sum += i
-
-print(f'Sum of even numbers is {sum}')
+sum_even = sum(i for i in rand_list if i % 2 == 0)
+print(f'Sum of even numbers is {sum_even}')

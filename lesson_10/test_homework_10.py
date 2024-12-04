3cf9fa92a31b7ab36891098a43ec8bee8e429b75
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-12-04 08:04:58 +0200

    add-parametrize

--- lesson_10/test_homework_10.py
+++ lesson_10/test_homework_10.py
@@ -8,50 +8,15 @@ def read_last_log_line():
         lines = log_file.readlines()
     return lines[-1].strip()
 
-
+@pytest.mark.parametrize('username, status_test', [
+  ('user1', 'success'),
+  ('user2', 'expired'),
+  ('user3', 'failed'),
+  ('user4', 'invalid_status'),
+])
 @pytest.mark.log_status_check
 @pytest.mark.positive
-def test_log_event_success():
-    username = "user1"
-    status_test = "success"
-
+def test_log_event_success(username, status_test):
     log_event(username, status_test)
     last_line = read_last_log_line()
-
-    assert f"Login event - Username: {username}, Status: {status_test}" in last_line
-
-
-@pytest.mark.log_status_check
-@pytest.mark.positive
-def test_log_event_expired():
-    username = "user2"
-    status_test = "expired"
-
-    log_event(username, status_test)
-    last_line = read_last_log_line()
-
-    assert f"Login event - Username: {username}, Status: {status_test}" in last_line
-
-
-@pytest.mark.log_status_check
-@pytest.mark.positive
-def test_log_event_failed():
-    username = "user3"
-    status_test = "failed"
-
-    log_event(username, status_test)
-    last_line = read_last_log_line()
-
-    assert f"Login event - Username: {username}, Status: {status_test}" in last_line
-
-
-@pytest.mark.log_status_check
-@pytest.mark.negative
-def test_log_event_invalid_status():
-    username = "user4"
-    status_test = "invalid_status"
-
-    log_event(username, status_test)
-    last_line = read_last_log_line()
-
     assert f"Login event - Username: {username}, Status: {status_test}" in last_line

9391258416e13133b16453308893f9a76ad3b35b
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-12-17 19:39:09 +0200

    small-fix

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -1,7 +1,7 @@
 # Створіть клас "Студент" з атрибутами "ім'я", "прізвище", "вік" та "середній бал". 
-# Створіть об'єкт цього класу, представляючи студента. Потім додайте метод до класу 
-# "Студент", який дозволяє змінювати середній бал студента. Виведіть інформацію про 
-# студента та змініть його середній бал.
+# Створіть об'єкт цього класу, представляючи студента. Потім додайте метод до класу "Студент", 
+# який дозволяє змінювати середній бал студента. Виведіть інформацію про студента та змініть його середній бал.
+
 
 class Student:
     def __init__(self, name, surname, age, avg_mark):
@@ -10,22 +10,15 @@ class Student:
         self.age = age
         self.avg_mark = avg_mark
 
-    def change_avg_mark(self, new_avg_mark):
-        self.avg_mark = new_avg_mark
+    def change_avg_mark(self, new_mark):
+        self.avg_mark = new_mark
 
     def print_student_info(self):
-        print(f"Ім'я: {self.name}")
-        print(f"Прізвище: {self.surname}")
-        print(f"Вік: {self.age}")
-        print(f"Середній бал: {self.avg_mark}")
+        print(f"{self.name} {self.surname} має середній бал {self.avg_mark}.")
+        print(f"Студенту {self.age} років.")
 
 
 student1 = Student("Олівер", "Томсон", 18, 88)
-
-print("\nІнформація про студента до зміни балу:")
-student1.print_student_info()
-
 student1.change_avg_mark(99)
 
-print("\nІнформація про студента після зміни балу:")
 student1.print_student_info()

9279ae1144fbe19a2e7433505e4698f196d33cdb
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-12-17 19:19:36 +0200

    hw-13

---
+++ lesson_13/homework_13_3.py
@@ -0,0 +1,4 @@
+# Завдання 3:
+
+# Для файла ideas_for_test/work_with_xml/groups.xml створіть функцію пошуку по group/number і 
+# повернення значення timingExbytes/incoming результат виведіть у консоль через логер на рівні інфо

90d39bb29f1ab4ccf528e3cb97e8e5320f6d078b
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-12-17 21:01:27 +0200

    add-second-part

--- lesson_13/homework_13_2.py
+++ lesson_13/homework_13_2.py
@@ -1,5 +1,24 @@
 # Завдання 2:
-
 # Провалідуйте, чи усі файли у папці ideas_for_test/work_with_json є валідними json. 
 # результат для невалідного файлу виведіть через логер на рівні еррор у файл json__<your_second_name>.log
 
+import json
+import logging
+
+
+logging.basicConfig(
+    filename='json__mishchenko.log',
+    level=logging.ERROR,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+def check_json(file_name):
+    with open(file_name, 'r') as file:
+        json_string = json.load(file)
+    
+    try:
+        data = json.loads(json_string)
+        print(data)
+    except json.JSONDecodeError as e:
+        logging.error(f"Файл {file_name} не є валідним JSON: {e}")

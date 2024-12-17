48390d5962158c3724ae73726ee07dc83be8e3d7
Anastasiia Mishchenko <mishenko971110@gmail.com>
2024-12-17 19:55:09 +0200

    part-one

--- lesson_13/homework_13_1.py
+++ lesson_13/homework_13_1.py
@@ -4,3 +4,23 @@
 # приберіть їх. Результат запишіть у файл result_<your_second_name>.csv
 
 
+import csv
+
+
+def open_csv(file_name):
+    with open(file_name, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print(', '.join(row))
+    return 0
+
+
+def check_dublicates():
+    return 0
+
+
+def write_csv(data):
+    with open('result_mishchenko.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+

c7b9ed01d1712d5878ac88757e0766f575bf35b8
Anastasiia Mishchenko <mishenko971110@gmail.com>
2025-01-04 21:56:36 +0200

    add-new-fix

--- lesson_13/homework_13_3.py
+++ lesson_13/homework_13_3.py
@@ -1,4 +1,42 @@
 # Завдання 3:
-
 # Для файла ideas_for_test/work_with_xml/groups.xml створіть функцію пошуку по group/number і 
 # повернення значення timingExbytes/incoming результат виведіть у консоль через логер на рівні інфо
+
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+def get_xml_file_list(parent_dir):
+    extension = '.xml'
+    file_list = []
+    files_with_extension = [f for f in parent_dir.iterdir() if f.suffix == extension]
+    # Виведення списку файлів з певним розширенням
+    print(f"Список файлів з розширенням '{extension}':")
+    for file in files_with_extension:
+        file_list.append(file)
+    return file_list
+
+
+def find_by_group(file_name):
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    # Читання та виведення даних з елементів XML-документу
+    for child in root:
+        print(child.tag, child.attrib)
+        for subchild in child:
+            print(subchild.tag, subchild.text)
+
+
+
+parent_dir = Path('./lesson_13/ideas_for_test/work_with_xml')
+file_list = get_xml_file_list(parent_dir)
+for file in file_list:
+    print(file)
+    if 'groups.xml' in str(file):
+        try:
+            find_by_group(file)
+        except ET.ParseError as e:
+            print(f"Помилка парсингу XML у файлі {file}")
+        except FileNotFoundError:
+            print(f"Файл не знайдено: {file}")
+        except Exception as e:
+            print(f"Невідома помилка у файлі {file}")

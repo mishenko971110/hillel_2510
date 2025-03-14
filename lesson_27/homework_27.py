2aad5197ad3113d5048396404ae8d46e0befcdd2
Anastasiia Mishchenko <mishenko971110@gmail.com>
2025-03-14 23:26:17 +0200

    add-tests-fix

--- lesson_27/homework_27.py
+++ lesson_27/homework_27.py
@@ -3,16 +3,16 @@
 ввести номер накладної (передається з тесту) та отримує статус посилки в теркінгу.
 Тест повинен перівіряти, що отриманий статус відповідає очікуваному.
 '''
-from selenium import webdriver
-from tracking_page import TrackingPage
+# from selenium import webdriver
+# from tracking_page import TrackingPage
 
-def check_post_status(post_URL, post_id):
-    driver = webdriver.Chrome()
-    tracking_page = TrackingPage(driver)
+# def check_post_status(post_URL, post_id):
+#     driver = webdriver.Chrome()
+#     tracking_page = TrackingPage(driver)
 
-    try:
-        tracking_page.open_url(post_URL)
-        tracking_page.track_package(post_id)
-        return tracking_page.get_tracking_status()
-    finally:
-        driver.quit()
+#     try:
+#         tracking_page.open_url(post_URL)
+#         tracking_page.track_package(post_id)
+#         return tracking_page.get_tracking_status()
+#     finally:
+#         driver.quit()

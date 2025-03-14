a11ccb523752b74c5ce1988b21665d66fb52ee5b
Anastasiia Mishchenko <mishenko971110@gmail.com>
2025-03-14 23:30:26 +0200

    commit-tests

--- lesson_28/test_registration.py
+++ lesson_28/test_registration.py
@@ -1,32 +1,32 @@
-import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from auth_page import AuthPage
-from auth_locators import AuthXpathLocators, AuthIdLocators
+# import pytest
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from auth_page import AuthPage
+# from auth_locators import AuthXpathLocators, AuthIdLocators
 
-@pytest.mark.parametrize("name, lastname, email, password", [
-    ("test", "test", "test@gmail.com", "Qwerty123!")
-])
+# @pytest.mark.parametrize("name, lastname, email, password", [
+#     ("test", "test", "test@gmail.com", "Qwerty123!")
+# ])
 
 
-def test_user_registration(driver, name, lastname, email, password):
-    auth_page = AuthPage(driver)
-    auth_page.open_signup_form()
+# def test_user_registration(driver, name, lastname, email, password):
+#     auth_page = AuthPage(driver)
+#     auth_page.open_signup_form()
     
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(AuthIdLocators.name_input)
-    )
+#     WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located(AuthIdLocators.name_input)
+#     )
 
-    auth_page.fill_registration_form(name, lastname, email, password)
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(AuthXpathLocators.submit_btn)
-    )
+#     auth_page.fill_registration_form(name, lastname, email, password)
+#     WebDriverWait(driver, 10).until(
+#         EC.element_to_be_clickable(AuthXpathLocators.submit_btn)
+#     )
 
-    auth_page.submit_registration()
-    actual_result = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(AuthXpathLocators.error_msg)
-    ).text
+#     auth_page.submit_registration()
+#     actual_result = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located(AuthXpathLocators.error_msg)
+#     ).text
 
-    actual_result = auth_page.get_error_msg()
-    expected_result = 'User already exists'
-    assert actual_result == expected_result
+#     actual_result = auth_page.get_error_msg()
+#     expected_result = 'User already exists'
+#     assert actual_result == expected_result
